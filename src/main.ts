10b9a3fa57eecc0b7dc756dad600dde379de9f71
Krzysztof <krzysztofjustgdynia@gmail.com>
2026-02-27 10:01:48 +0100

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Chat API')
+    .setDescription('Api documentation for chat application')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
+  await app.listen(process.env.PORT ?? 5000)
 }
-bootstrap();
+bootstrap()
